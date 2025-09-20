841658903c76b485b78de8db85a31423b9109dc8
iamaditya13 <imaditya4work@gmail.com>
2025-09-20 15:22:38 +0530

    final push

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
